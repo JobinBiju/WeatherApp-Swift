420b4244c9243db9b2c34b4a956011d90980a6f3
JobinBiju <jobinbiju9090@gmail.com>
2025-03-28 12:31:54 +0530

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Jobin Biju on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
